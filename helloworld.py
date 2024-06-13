746733138be4d9ae468e5abb73199517058662a1
krobed <73363947+krobed@users.noreply.github.com>
2024-06-12 21:59:40 -0400

    Update helloworld.py
    Added rock paper scissors game with python code through rps() function, in addition to hiworld() function.

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,22 @@
-print('Hello World')
+import numpy as np
+
+
+def hiworld():
+  print('Hello World')
+
+def rps():
+  play = input('Choose between rock, paper or scissors (r, p or s): ')
+  r = np.random.randint(0,2)
+  d = ['rock', 'paper','scissors']
+  out= d[r]
+  if out[0] == play:
+    print(f'{out}, it's a draw!')
+  elif out == 'rock' and play=='p':
+    print f'{out}, you lost!'
+  elif play == 'p' and out=='scissors':
+    print f'{out}, you lose!'
+  else:
+    print f'{out}, you win!
+  again= input('Play again? (y/~): ')
+  if again=='y':
+    rps()

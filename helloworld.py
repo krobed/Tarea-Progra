3df5abcf1ca4ed48c935a26ef9a8fa75942e509f
krobed <73363947+krobed@users.noreply.github.com>
2024-06-19 21:27:17 -0400

    Update helloworld.py
    Se agregó la función rps() para jugar piedra, papel y tijeras con la máquina.

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,10 @@
 import numpy as np
 
-
+#Hello world function
 def hiworld():
   print('Hello World')
 
+#Rock paper scissors function
 def rps():
   play = input('Choose between rock, paper or scissors (r, p or s): ')
   r = np.random.randint(0,2)

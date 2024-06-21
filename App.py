6a077d93bde3911d01e283ea925631a8d8fde64c
krobed <73363947+krobed@users.noreply.github.com>
2024-06-21 16:09:13 -0400

    Update App.py
    add test functions

--- App.py
+++ App.py
@@ -60,9 +60,18 @@ def is_leap_year(year: int) -> bool:
     else:
         print(str(year), "is not a leap year.")
 
-assert is_leap_year(2024)== 'is a leap year.'
-assert hiworld() == 'Hello World'
-assert rps('rock') == "Invalid input. Please enter 'r' for rock, 'p' for paper, or 's' for scissors."
-assert type(print_date()) == str
-# Example usage
+
+#Tests
+def test_rps():
+  assert rps('rock') == "Invalid input. Please enter 'r' for rock, 'p' for paper, or 's' for scissors."
+
+def test_print_date():
+  assert type(print_date()) == str
+
+def test_is_leap_year():
+  assert is_leap_year(2024)== 'is a leap year.'
+
+
+def test_hiworld():
+  assert hiworld() == 'Hello World'
 

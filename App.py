61dad529559fe2292621f0001437158736242b8c
krobed <73363947+krobed@users.noreply.github.com>
2024-06-21 16:15:07 -0400

    Update App.py

--- App.py
+++ App.py
@@ -61,17 +61,4 @@ def is_leap_year(year: int) -> bool:
         print(str(year), "is not a leap year.")
 
 
-#Tests
-def test_rps():
-  assert rps('rock') == "Invalid input. Please enter 'r' for rock, 'p' for paper, or 's' for scissors."
-
-def test_print_date():
-  assert type(print_date()) == str
-
-def test_is_leap_year():
-  assert is_leap_year(2024)== 'is a leap year.'
-
-
-def test_hiworld():
-  assert hiworld() == 'Hello World'
 
